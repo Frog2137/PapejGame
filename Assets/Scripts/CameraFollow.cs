5aa4bc70287df62659bbaddb101fea98a9ca634c
Frog2137 <filipekopo@gmail.com>
2021-08-20 15:14:47 +0200

    village update

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 2, 0);
+    }
+}
